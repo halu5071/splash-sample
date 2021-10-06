34ac2a42cc05895973625d4c95360596da4427a7
Yasunori <horiiortho5@gmail.com>
2021-10-06 22:10:19 +0900

    Initial commit
    This project was created by template 'Empty Activity'
    on Android Studio Arctic Fox patch 2.
    
    More environment info:
    
    Android Studio Arctic Fox | 2020.3.1 Patch 2
    Build #AI-203.7717.56.2031.7678000, built on August 27, 2021
    Runtime version: 11.0.10+0-b96-7281165 x86_64
    VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
    macOS 11.5.2
    GC: G1 Young Generation, G1 Old Generation
    Memory: 2048M
    Cores: 6
    Registry: external.system.auto.import.disabled=true
    Non-Bundled Plugins: IdeaVIM, org.jetbrains.kotlin, com.jetbrains.kmm

---
+++ app/src/main/java/io/moatwel/sample/splash/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.moatwel.sample.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
