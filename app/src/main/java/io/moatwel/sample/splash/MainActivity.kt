6fb67340d8921882129f6aadccd0f9428b7055d7
Yasunori <horiiortho5@gmail.com>
2021-10-07 00:01:15 +0900

    Add splash screen compatible with Android 12
    Add androidx.core:core-splashscreen:1.0.0-alpha02
    and apply Theme.SplashScreen on MainActivity.

--- app/src/main/java/io/moatwel/sample/splash/MainActivity.kt
+++ app/src/main/java/io/moatwel/sample/splash/MainActivity.kt
@@ -2,10 +2,26 @@ package io.moatwel.sample.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private var appReady = false
+
+    companion object {
+        private const val DELAY_MILLI_SEC = 2000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepVisibleCondition { appReady.not() }
+        }
+
+        Handler(mainLooper).postDelayed({ appReady = true }, DELAY_MILLI_SEC)
+
         setContentView(R.layout.activity_main)
     }
 }
